3d6f7e2f75fc13640ac181ab6006519b030641cc
Documenter.jl <documenter@juliadocs.github.io>
2021-04-29 11:03:00 +0000

    build based on 5b440e0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FRGRealTime","category":"page"},{"location":"#FRGRealTime","page":"Home","title":"FRGRealTime","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FRGRealTime.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FRGRealTime]","category":"page"}]
+}
