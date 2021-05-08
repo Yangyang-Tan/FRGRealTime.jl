98cfc93be50a6f1b5ecc015c71396216d2c80b1d
Yang-yang Tan <1395935398@qq.com>
2021-05-08 15:48:38 +0800

    del

--- docs/build/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"index.html","page":"Home","title":"Home","text":"CurrentModule = FRGRealTime","category":"page"},{"location":"index.html#FRGRealTime","page":"Home","title":"FRGRealTime","text":"","category":"section"},{"location":"index.html","page":"Home","title":"Home","text":"Documentation for FRGRealTime.","category":"page"},{"location":"index.html","page":"Home","title":"Home","text":"","category":"page"},{"location":"index.html","page":"Home","title":"Home","text":"Modules = [FRGRealTime]","category":"page"},{"location":"index.html#FRGRealTime.Epi-Tuple{Any, Any}","page":"Home","title":"FRGRealTime.Epi","text":"Epi(k,m) compute sqrt (x^2+m)\n\n\n\n\n\n","category":"method"}]
-}
