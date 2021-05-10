0d2a6921b0692b893cee817c7c6297b07db1f682
Yang-yang Tan <1395935398@qq.com>
2021-05-10 17:22:59 +0800

    add

--- src/vertex.jl
+++ src/vertex.jl
@@ -53,24 +53,24 @@ VImintqs(p0, ps, k, T, Npi,mfun,lampifun) =
     )[1] +deltasumkAll(p0 + Epi(k, msgfun2(k)), ps, k, T, Npi) + deltasumkAll(p0 - Epi(k, msgfun2(k)), ps, k, T, Npi)
 
 
-
-propImsimpleintqs(p0, ps, T, Npi) =
-    -hcubature(
-        k ->
-            (
-                (V4piImintqs(p0, ps, k[1], T, Npi)) *
-                k[1] *
-                (
-                    -(
-                        coth(Epi(k[1], msgfun2(k[1])) / (2 * T)) /
-                        Epi(k[1], msgfun2(k[1]))^3
-                    ) -
-                    csch(Epi(k[1], msgfun2(k[1])) / (2 * T))^2 /
-                    (2 * T * Epi(k[1], msgfun2(k[1]))^2)
-                )
-            ) / (16 * pi^2),
-        [kmin],
-        [Λ],
-        rtol = 1e-8,
-        atol = 1e-8,maxevals=8000
-    )[1]
+# 
+# propImsimpleintqs(p0, ps, T, Npi) =
+#     -hcubature(
+#         k ->
+#             (
+#                 (V4piImintqs(p0, ps, k[1], T, Npi)) *
+#                 k[1] *
+#                 (
+#                     -(
+#                         coth(Epi(k[1], msgfun2(k[1])) / (2 * T)) /
+#                         Epi(k[1], msgfun2(k[1]))^3
+#                     ) -
+#                     csch(Epi(k[1], msgfun2(k[1])) / (2 * T))^2 /
+#                     (2 * T * Epi(k[1], msgfun2(k[1]))^2)
+#                 )
+#             ) / (16 * pi^2),
+#         [kmin],
+#         [Λ],
+#         rtol = 1e-8,
+#         atol = 1e-8,maxevals=8000
+#     )[1]
