b6d77f788012e078e536c1660c894735bcacf3a0
Yang-yang Tan <1395935398@qq.com>
2021-05-10 16:04:54 +0800

    add

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,9 +7,3 @@ using FiniteDifferences
     @test FRGRealTime.flowpp(1.1, 1.0, 5.0, -2.0, 10.0)==central_fdm(5, 1)(k->FRGRealTime.loopfunpp(1.1, 1.0, k, -2.0, 10.0),5.0)
     @test FRGRealTime.flowpp(10.0, 1.0, 5.0, -2.0, 10.0)≈central_fdm(5, 1)(k->FRGRealTime.loopfunpp(10.0, 1.0, k, -2.0, 10.0),5.0)
 end
-
-FRGRealTime.loopfunpm(1e-12,1e-11,1.0,0.5,2.0)
-using Plots
-
-
-plot(x->FRGRealTime.loopfunpm(x,1e-10,1.0,0.5,2.0),1e-12,1e-10)
