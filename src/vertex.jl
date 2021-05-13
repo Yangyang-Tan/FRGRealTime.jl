ab23847205124e2d4850d93e5121f6f12c11315a
Yang-yang Tan <1395935398@qq.com>
2021-05-13 19:12:29 +0800

    add

--- src/vertex.jl
+++ src/vertex.jl
@@ -99,7 +99,7 @@ end
 
 
 #integrate delta function in F1 , we integrate out qs, cos(θ) and k' this will be used in Im part calculation
-function deltasumImprop1(p0, ps, k, T, Npi, IRScale, UVScale, mfun, lamfun)
+function deltasumImprop1(p0, ps, T,IRScale,UVScale, Npi, mfun, lamfun)
     #find the location of k0 where p0==2Epi(k,m)
     deltaf(x) = 3 * Epi(x, mfun(x)) - p0
     if deltaf(IRScale) * deltaf(UVScale) >= 0
@@ -112,36 +112,111 @@ function deltasumImprop1(p0, ps, k, T, Npi, IRScale, UVScale, mfun, lamfun)
             return 0.0
             #δ function only appears in p<2k so we have the following division
         elseif IRScale <= k0
-            if ps <= 2 * k0 - k
-                if ps > k
-                    return (
-                        k^3 *
-                        k0 *
-                        (k^2 + 5 * ps * (-2 * k0 + ps)) *
-                        coth(p0 / (4 * T)) *
-                        lamfun(k0)^2
-                    ) / (
-                        30 *
-                        p0 *
-                        pi^2 *
-                        ps *
-                        abs(2 * k0 + derivative(mfun, k0))
-                    )
-                elseif ps <= k
-                    return -1 / 120 * (
-                        k0 *
-                        (5 * k^3 * (-3 * k + 8 * k0) - 10 * k^2 * ps^2 + ps^4) *
-                        coth(p0 / (4 * T)) *
-                        lamfun(k0)^2
-                    ) / (p0 * pi^2 * abs(2 * k0 + derivative(mfun, k0)))
-                end
+            if ps < k0
+                return (
+                    3 *
+                    k0^2 *
+                    (2 + Npi) *
+                    coth(p0 / (6 * T)) *
+                    csch(p0 / (6 * T))^2 *
+                    lamfun(k0)^2 *
+                    (
+                        3075 * k0^4 - 1900 * k0^2 * ps^2 +
+                        137 * ps^4 +
+                        60 * ps^2 * (-20 * k0^2 + ps^2) * log(k0 / ps)
+                    ) *
+                    sign(2 * k0 + derivative(mfun, k0)) *
+                    (p0 + 3 * T * sinh(p0 / (3 * T)))
+                ) / (
+                    25600 * p0^4 * pi^3 * T * (2 * k0 + derivative(mfun, k0))^2
+                )
+            elseif k0 < ps <= 2 * k0
+                return (
+                    3 *
+                    k0^2 *
+                    (2 + Npi) *
+                    coth(p0 / (6 * T)) *
+                    csch(p0 / (6 * T))^2 *
+                    lamfun(k0)^2 *
+                    (
+                        (3 * k0 - ps) * (
+                            132 * k0^4 + 3099 * k0^3 * ps - 2047 * k0^2 * ps^2 -
+                            9 * k0 * ps^3 + 137 * ps^4
+                        ) +
+                        60 *
+                        (2 * k0 - ps)^3 *
+                        (4 * k0^2 + 6 * k0 * ps + ps^2) *
+                        log(k0 / (2 * k0 - ps))
+                    ) *
+                    sign(2 * k0 + derivative(mfun, k0)) *
+                    (p0 + 3 * T * sinh(p0 / (3 * T)))
+                ) / (
+                    51200 *
+                    p0^4 *
+                    pi^3 *
+                    ps *
+                    T *
+                    (2 * k0 + derivative(mfun, k0))^2
+                )
+            elseif 2*k0 < ps <= 3 * k0
+                return (
+                    -3 *
+                    k0^2 *
+                    (2 + Npi) *
+                    coth(p0 / (6 * T)) *
+                    csch(p0 / (6 * T))^2 *
+                    lamfun(k0)^2 *
+                    (
+                        -(
+                            (3 * k0 - ps) * (
+                                132 * k0^4 + 3099 * k0^3 * ps -
+                                2047 * k0^2 * ps^2 - 9 * k0 * ps^3 + 137 * ps^4
+                            )
+                        ) +
+                        60 *
+                        (2 * k0 - ps)^3 *
+                        (4 * k0^2 + 6 * k0 * ps + ps^2) *
+                        log(-2 + ps / k0)
+                    ) *
+                    sign(2 * k0 + derivative(mfun, k0)) *
+                    (p0 + 3 * T * sinh(p0 / (3 * T)))
+                ) / (
+                    51200 *
+                    p0^4 *
+                    pi^3 *
+                    ps *
+                    T *
+                    (2 * k0 + derivative(mfun, k0))^2
+                )
             elseif ps > 3 * k0
                 return 0.0
-            elseif 2*k < ps < 2 * k0 + k
-                # println("locate end")
-                return (k0*(k+2*k0-ps)^3*(6*k0^2+(k-ps)*(4*k+ps)-k0*(9*k+ps))*coth(p0/(4*T))*lamfun(k0)^2)/(240*p0*pi^2*ps*abs(2*k0+derivative(mfun,k0)))
-            elseif 2 * k0 - k<ps < 2 * k0
-                return (k0*(k+2*k0-ps)^3*(6*k0^2+(k-ps)*(4*k+ps)-k0*(9*k+ps))*coth(p0/(4*T))*lamfun(k0)^2)/(240*p0*pi^2*ps*abs(2*k0+derivative(mfun,k0)))
+            end
+        end
+    end
+end
+
+
+function deltasumImprop2(p0, ps, T,IRScale,UVScale, Npi, mfun, lamfun)
+    #find the location of k0 where p0==2Epi(k,m)
+    deltaf(x) = Epi(x, mfun(x)) - p0
+    if deltaf(IRScale) * deltaf(UVScale) >= 0
+        return 0.0
+    else
+        k0 = find_zero(deltaf, (IRScale, UVScale))
+        # println(" k0=", k0)
+        # k0 should lies between k~Λ, when k>k0 you will get 0
+        if IRScale > k0
+            return 0.0
+            #δ function only appears in p<2k so we have the following division
+        elseif IRScale <= k0
+            if ps < k0
+                return (k0^2*(2+Npi)*csch(p0/(2*T))^4*lamfun(k0)^2*(3075*k0^4-1900*k0^2*ps^2+137*ps^4+60*ps^2*(-20*k0^2+ps^2)*log(k0/ps))*sign(2*k0+derivative(mfun,k0))*sinh(p0/T)*(p0+T*sinh(p0/T)))/(153600*p0^4*pi^3*T*(2*k0+derivative(mfun,k0))^2)
+            elseif k0 < ps <= 2 * k0
+                return (k0^2*(2+Npi)*csch(p0/(2*T))^4*lamfun(k0)^2*((3*k0-ps)*(132*k0^4+3099*k0^3*ps-2047*k0^2*ps^2-9*k0*ps^3+137*ps^4)+60*(2*k0-ps)^3*(4*k0^2+6*k0*ps+ps^2)*log(k0/(2*k0-ps)))*sign(2*k0+derivative(mfun,k0))*sinh(p0/T)*(p0+T*sinh(p0/T)))/(307200*p0^4*pi^3*ps*T*(2*k0+derivative(mfun,k0))^2)
+            elseif 2*k0 < ps <= 3 * k0
+                return (k0^2*(2+Npi)*csch(p0/(2*T))^4*lamfun(k0)^2*((3*k0-ps)*(132*k0^4+3099*k0^3*ps-2047*k0^2*ps^2-9*k0*ps^3+137*ps^4)+60*(2*k0-ps)^3*(4*k0^2+6*k0*ps+ps^2)*log(k0/(2*k0-ps)))*sign(2*k0+derivative(mfun,k0))*sinh(p0/T)*(p0+T*sinh(p0/T)))/(307200*p0^4*pi^3*ps*T*(2*k0+derivative(mfun,k0))^2)
+            elseif ps > 3 * k0
+                return 0.0
             end
         end
     end
