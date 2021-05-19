dc6f99279ccc969fb157f31222eeab8a93c063c1
Yang-yang Tan <1395935398@qq.com>
2021-05-19 15:14:14 +0800

    add

--- src/Re.jl
+++ src/Re.jl
@@ -102,10 +102,9 @@ function PvdkF1Tildeps(p0, ps, qsmax, k, m, T; kwargs...)
             p0,
             Epi(k + ps + qsmax, m) + Epi(k, m);
             kwargs...,
-        )
-        # +
-        # delta1_intcosthqs(p0, ps, qsmax, k, m, T) +
-        # delta2_intcosthqs(p0, ps, qsmax, k, m, T)
+        )+
+        delta1_intcosthqs(p0, ps, qsmax, k, m, T) +
+        delta2_intcosthqs(p0, ps, qsmax, k, m, T)
     end
 end
 
